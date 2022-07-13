e291106e8ecc5d0fa06abf30fd6a985e2ce99193
Leo_Amv <leonidamvrosov@yandex.ru>
2022-07-14 02:02:41 +0500

    Task 2 completed

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,17 +2,32 @@
 {
     class Task43 // Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+    // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
     {
-        public static void Main(string [] args)
+        public static void Main()
         {
             try
             {
-                
+                Console.WriteLine("Enter number b1 : ");
+                float b1 = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Enter number k1 : ");
+                float k1 = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Enter number b2 : ");
+                float b2 = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Enter number k2 : ");
+                float k2 = Convert.ToSingle(Console.ReadLine());
+                if (k1 == k2) Console.WriteLine("Intersection point does not exist! Lines are parallel!");
+                else
+                {
+                    float x = (b2 - b1) / (k1 - k2);
+                    float y = k1 * x + b1;
+                    Console.WriteLine($"Intersection point is : \t ({x} ; {y})");
+                }
             }
-            catch (System.Exception)
+            catch
             {
-                
+                Console.WriteLine("INCORRECT DATA ! Please enter the numbers ");
+                Main();
             }
         }
     }
